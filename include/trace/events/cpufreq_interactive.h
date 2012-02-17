965dfe1ab9d2277c9b1a01b0d1de1848d0746506
Todd Poynor <toddpoynor@google.com>
2012-02-16 16:27:59 -0800

    cpufreq interactive governor: event tracing
    Change-Id: Ic13614a3da2faa2d4bd215ca3eb7191614f0cf66
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- include/trace/events/cpufreq_interactive.h
+++ include/trace/events/cpufreq_interactive.h
@@ -21,8 +21,12 @@ DECLARE_EVENT_CLASS(set,
 	      __entry->cpu_id, __entry->targfreq,
 	      __entry->actualfreq)
 );
-
-DEFINE_EVENT(set, cpufreq_interactive_setspeed,
+DEFINE_EVENT(set, cpufreq_interactive_up,
+	TP_PROTO(u32 cpu_id, unsigned long targfreq,
+	     unsigned long actualfreq),
+	TP_ARGS(cpu_id, targfreq, actualfreq)
+);
+DEFINE_EVENT(set, cpufreq_interactive_down,
 	TP_PROTO(u32 cpu_id, unsigned long targfreq,
 	     unsigned long actualfreq),
 	TP_ARGS(cpu_id, targfreq, actualfreq)
@@ -62,31 +66,6 @@ DEFINE_EVENT(loadeval, cpufreq_interactive_notyet,
 		     unsigned long curfreq, unsigned long targfreq),
 	    TP_ARGS(cpu_id, load, curfreq, targfreq)
 );
-
-TRACE_EVENT(cpufreq_interactive_boost,
-	    TP_PROTO(char *s),
-	    TP_ARGS(s),
-	    TP_STRUCT__entry(
-		    __field(char *, s)
-	    ),
-	    TP_fast_assign(
-		    __entry->s = s;
-	    ),
-	    TP_printk("%s", __entry->s)
-);
-
-TRACE_EVENT(cpufreq_interactive_unboost,
-	    TP_PROTO(char *s),
-	    TP_ARGS(s),
-	    TP_STRUCT__entry(
-		    __field(char *, s)
-	    ),
-	    TP_fast_assign(
-		    __entry->s = s;
-	    ),
-	    TP_printk("%s", __entry->s)
-);
-
 #endif /* _TRACE_CPUFREQ_INTERACTIVE_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
