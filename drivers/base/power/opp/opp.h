be866bf0b79fd60891014aef9a4168c10a777cf3
Viresh Kumar <viresh.kumar@linaro.org>
2015-12-09 08:01:47 +0530

    PM / OPP: Parse 'opp-<prop>-<name>' bindings
    OPP bindings (for few properties) allow a platform to choose a
    value/range among a set of available options. The options are present as
    opp-<prop>-<name>, where the platform needs to supply the <name> string.
    
    The OPP properties which allow such an option are: opp-microvolt and
    opp-microamp.
    
    Add support to the OPP-core to parse these bindings, by introducing
    dev_pm_opp_{set|put}_prop_name() APIs.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Tested-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 01fb4d3c39d35b725441e8a9a26b3f3ad67793ed)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>
    
    Conflicts:
    	rm debugfs part in drivers/base/power/opp/opp.h

--- drivers/base/power/opp/opp.h
+++ drivers/base/power/opp/opp.h
@@ -114,6 +114,12 @@ struct device_list_opp {
  * @opp_list:	list of opps
  * @np:		struct device_node pointer for opp's DT node.
  * @shared_opp: OPP is shared between multiple devices.
+ * @suspend_opp: Pointer to OPP to be used during device suspend.
+ * @supported_hw: Array of version number to support.
+ * @supported_hw_count: Number of elements in supported_hw array.
+ * @prop_name: A name to postfix to many DT properties, while parsing them.
+ * @dentry:	debugfs dentry pointer of the real device directory (not links).
+ * @dentry_name: Name of the real dentry.
  *
  * This is an internal data structure maintaining the link to opps attached to
  * a device. This structure is not meant to be shared to users as it is
@@ -138,6 +144,8 @@ struct device_opp {
 
 	unsigned int *supported_hw;
 	unsigned int supported_hw_count;
+	const char *prop_name;
+
 };
 
 /* Routines internal to opp core */
