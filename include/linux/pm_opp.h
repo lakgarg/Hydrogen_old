64777d1d3c116a0e51b08e8b449efed48d08a556
Viresh Kumar <viresh.kumar@linaro.org>
2015-12-09 08:01:46 +0530

    PM / OPP: Parse 'opp-supported-hw' binding
    OPP bindings allow a platform to enable OPPs based on the version of the
    hardware they are used for.
    
    Add support to the OPP-core to parse these bindings, by introducing
    dev_pm_opp_{set|put}_supported_hw() APIs.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Tested-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 7de36b0aa51a5a59e28fb2da768fa3ab07de0674)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

--- include/linux/pm_opp.h
+++ include/linux/pm_opp.h
@@ -60,11 +60,6 @@ struct srcu_notifier_head *dev_pm_opp_get_notifier(struct device *dev);
 int dev_pm_opp_set_supported_hw(struct device *dev, const u32 *versions,
 				unsigned int count);
 void dev_pm_opp_put_supported_hw(struct device *dev);
-int dev_pm_opp_set_prop_name(struct device *dev, const char *name);
-void dev_pm_opp_put_prop_name(struct device *dev);
-int dev_pm_opp_set_regulator(struct device *dev, const char *name);
-void dev_pm_opp_put_regulator(struct device *dev);
-int dev_pm_opp_set_rate(struct device *dev, unsigned long target_freq);
 #else
 static inline unsigned long dev_pm_opp_get_voltage(struct dev_pm_opp *opp)
 {
@@ -158,25 +153,6 @@ static inline int dev_pm_opp_set_supported_hw(struct device *dev,
 }
 
 static inline void dev_pm_opp_put_supported_hw(struct device *dev) {}
-
-static inline int dev_pm_opp_set_prop_name(struct device *dev, const char *name)
-{
-	return -EINVAL;
-}
-
-static inline void dev_pm_opp_put_prop_name(struct device *dev) {}
-
-static inline int dev_pm_opp_set_regulator(struct device *dev, const char *name)
-{
-	return -EINVAL;
-}
-
-static inline void dev_pm_opp_put_regulator(struct device *dev) {}
-
-static inline int dev_pm_opp_set_rate(struct device *dev, unsigned long target_freq)
-{
-	return -EINVAL;
-}
 
 #endif		/* CONFIG_PM_OPP */
 
