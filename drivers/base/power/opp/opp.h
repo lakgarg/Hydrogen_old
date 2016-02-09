432811aa9f5f9871dde38b2c611713636e59c824
Viresh Kumar <viresh.kumar@linaro.org>
2016-02-09 10:30:33 +0530

    PM / OPP: get/put regulators from OPP core
    This allows the OPP core to request/free the regulator resource,
    attached to a device OPP. The regulator device is fetched using the name
    provided by the driver, while calling: dev_pm_opp_set_regulator().
    
    This will work for both OPP-v1 and v2 bindings.
    
    This is a preliminary step for moving the OPP switching logic into the
    OPP core.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 9f8ea969d5cfdd4353d2adb004e8e2286b984369)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

--- drivers/base/power/opp/opp.h
+++ drivers/base/power/opp/opp.h
@@ -21,6 +21,8 @@
 #include <linux/rculist.h>
 #include <linux/rcupdate.h>
 
+struct regulator;
+
 /* Lock to allow exclusive modification to the device and opp lists */
 extern struct mutex dev_opp_list_lock;
 
@@ -118,6 +120,7 @@ struct device_list_opp {
  * @supported_hw: Array of version number to support.
  * @supported_hw_count: Number of elements in supported_hw array.
  * @prop_name: A name to postfix to many DT properties, while parsing them.
+ * @regulator: Supply regulator
  * @dentry:	debugfs dentry pointer of the real device directory (not links).
  * @dentry_name: Name of the real dentry.
  *
@@ -145,6 +148,7 @@ struct device_opp {
 	unsigned int *supported_hw;
 	unsigned int supported_hw_count;
 	const char *prop_name;
+	struct regulator *regulator;
 
 };
 
